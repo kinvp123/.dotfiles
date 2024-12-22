69ce6b1f584aab7a954ae351cf698c06543566d5
kinvp123 <kinvp123@proton.me>
2024-12-22 20:27:43 +0700

    supporting changes

--- modules/user/firefoxcfg.nix
+++ modules/user/firefoxcfg.nix
@@ -32,6 +32,11 @@ in
       DisplayBookmarksToolbar = "newtab"; # alternatives: "always" or "newtab"
       DisplayMenuBar = "default-off"; # alternatives: "always", "never" or "default-on"
       SearchBar = "unified"; # alternative: "separate"
+      Homepage = {
+        URL = "https://kinvp123.github.io/catppuccin-startpage/";
+        Locked = true;
+        StartPage = "homepage";
+      };
       ExtensionSettings = with builtins;
         let extension = pluginName: uuid: {
           name = uuid;
@@ -43,6 +48,8 @@ in
         in listToAttrs [ 
             (extension "ublock-origin" "uBlock0@raymondhill.net")
             (extension "privacy-badger17" "jid1-MnnxcxisBPnSXQ@jetpack")
+            (extension "sidetabs" "{ccc8cbaa-3c36-46d1-b0ae-d5e122755901}")
+            (extension "custom-new-tab-page" "custom-new-tab-page@mint.as")
         ];
       Preferences = {
           "browser.newtabpage.activity-stream.feeds.section.topstories" = state-false;
@@ -55,8 +62,9 @@ in
           "browser.newtabpage.activity-stream.system.showSponsored" = state-false;
           "browser.newtabpage.activity-stream.showSponsoredTopSites" = state-false;
           "toolkit.legacyUserProfileCustomizations.stylesheets" = state-true;
+          "reader.parse-on-load.enabled" = state-false;
       };
     };
   };
-  home.file.".mozilla/firefox/um3bx2d7.default/chrome/userChrome.css".source = ../theming/userFirefox.css;
+  home.file.".mozilla/firefox/um3bx2d7.default/chrome/userChrome.css".source = ../theming/styleFiles/userFirefox.css;
 }
